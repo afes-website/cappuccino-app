b83816f7f15ef576e9c50b21094ad0d3129d9291
subaru <s@su8ru.dev>
2020-08-10 23:22:03 +0900

    feat: prepare original palette

---
+++ src/assets/styles/origPalette.ts
@@ -0,0 +1,17 @@
+import { PaletteColorOptions } from "@material-ui/core";
+
+export const afesDark: PaletteColorOptions = {
+  light: "#def",
+  main: "#457fb3",
+  dark: "#358",
+};
+
+export const afesBlue: PaletteColorOptions = {
+  main: "#3399cc",
+};
+
+export const afesLight: PaletteColorOptions = {
+  main: "#def",
+};
+
+export default { afesDark, afesBlue, afesLight };
