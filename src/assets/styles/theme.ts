30ba0c3c4f022074f44070cb050acb9e01c4e768
subaru <s@su8ru.dev>
2020-08-11 23:22:46 +0900

    feat: prepare theme config

---
+++ src/assets/styles/theme.ts
@@ -0,0 +1,62 @@
+import { Theme, createMuiTheme } from "@material-ui/core";
+import { afesDark, afesLight } from "@/assets/styles/origPalette";
+import { PaletteOptions } from "@material-ui/core/styles/createPalette";
+import deepmerge from "deepmerge";
+import { ComponentsProps } from "@material-ui/core/styles/props";
+
+const lightPalette: PaletteOptions = {
+  primary: afesDark,
+  secondary: afesDark,
+};
+
+const darkPalette: PaletteOptions = {
+  type: "dark",
+  primary: afesDark,
+  secondary: afesLight,
+};
+
+const props: ComponentsProps = {
+  MuiCheckbox: {
+    color: "secondary",
+  },
+  MuiList: {
+    dense: true,
+  },
+  MuiRadio: {
+    color: "secondary",
+  },
+  MuiSwitch: {
+    color: "secondary",
+  },
+  MuiTable: {
+    size: "small",
+  },
+  MuiTextField: {
+    variant: "outlined",
+  },
+};
+
+const typography = {
+  fontSize: 12,
+  button: {
+    textTransform: "none",
+  },
+};
+
+export const themeLight: Theme = createMuiTheme(
+  deepmerge.all([
+    { palette: lightPalette },
+    { props: props },
+    { typography: typography },
+  ])
+);
+
+export const themeDark: Theme = createMuiTheme(
+  deepmerge.all([
+    { palette: darkPalette },
+    { props: props },
+    { typography: typography },
+  ])
+);
+
+export default { light: themeLight, dark: themeDark };
