fd3c7151937cdb861b1bab7e84fcc0eae591007a
ibuki2003 <main@fuwa.dev>
2020-08-15 01:52:12 +0900

    feat: add Link to scan page
    Co-authored-by: subaru <s@su8ru.dev>

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,19 +1,11 @@
 import React from "react";
-import {
-  Card,
-  CardContent,
-  Typography,
-  CardActions,
-  Button,
-} from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
+import routes from "@/libs/routes";
+import HomeCard from "@/components/homeCard";
 
 const useStyles = makeStyles({
   root: {
-    padding: "1rem",
-  },
-  title: {
-    marginBottom: "8px",
+    padding: "10px",
   },
 });
 
@@ -22,23 +14,22 @@ const Home: React.FunctionComponent = () => {
 
   return (
     <div className={classes.root}>
-      <Card variant="outlined">
-        <CardContent>
-          <Typography variant="h6" component="h2" className={classes.title}>
-            入退室 QRコードスキャン
-          </Typography>
-          <Typography variant="body2" paragraph={true}>
-            自展示への入退室者のリストバンドのQRコードを読み取ることで、入退室処理を行えます。
-          </Typography>
-          <Typography variant="body2" paragraph={true}>
-            画面に案内が出た場合は、表示された内容を来場者に案内してください。
-          </Typography>
-        </CardContent>
-        <CardActions>
-          <Button color="secondary">入室スキャン</Button>
-          <Button color="secondary">退室スキャン</Button>
-        </CardActions>
-      </Card>
+      <HomeCard
+        title="入退場 QRコードスキャン"
+        paragraphs={[
+          "自展示への入退室者のリストバンドのQRコードを読み取ることで、入退室処理を行えます。",
+          "画面に案内が出た場合は、表示された内容を来場者に案内してください。",
+        ]}
+        buttons={[
+          ["入室スキャン", routes.Scan],
+          ["退室スキャン", routes.Scan],
+        ]}
+      />
+      <HomeCard
+        title="QRコードスキャン履歴"
+        paragraphs={["自展示への来場者の入退室履歴を閲覧できます。"]}
+        buttons={[["履歴照会", routes.Scan]]}
+      />
     </div>
   );
 };
