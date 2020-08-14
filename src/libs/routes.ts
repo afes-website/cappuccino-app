4f2396df2507bb9ed25e8f5f58af9185f0ef0c53
ibuki2003 <main@fuwa.dev>
2020-08-15 01:47:03 +0900

    feat: add Scan page
    Co-authored-by: subaru <s@su8ru.dev>

--- src/libs/routes.ts
+++ src/libs/routes.ts
@@ -1,9 +1,11 @@
 import { typedRoute } from "@/components/typesafeRouter";
 import { route } from "typesafe-react-router";
 import Home from "@/pages/Home";
+import Scan from "@/pages/Scan";
 
 const routes = {
   Home: typedRoute(route(""), Home),
+  Scan: typedRoute(route("scan"), Scan),
 };
 
 export default routes;
