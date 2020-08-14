73507cddd56966f608195eec82d43889cd667d19
subaru <s@su8ru.dev>
2020-08-15 00:11:29 +0900

    feat: update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,44 @@ import React, { useState } from "react";
 import TypesafeRouter from "@/components/typesafeRouter";
 import routes from "@/libs/routes";
 import { createBrowserHistory } from "history";
+import {
+  createStyles,
+  makeStyles,
+  Paper,
+  ThemeProvider,
+} from "@material-ui/core";
 import NotFound from "@/pages/NotFound";
+import TopBar from "@/components/topBar";
+import BottomNav from "@/components/bottomNav";
+import themes from "@/assets/styles/theme";
+
+const useStyles = makeStyles(() =>
+  createStyles({
+    root: {
+      height: "100vh",
+    },
+    bottomNav: {
+      position: "fixed",
+      bottom: 0,
+      width: "100%",
+    },
+  })
+);
 
 const App: React.FunctionComponent = () => {
   const [history] = useState(createBrowserHistory());
+  const classes = useStyles();
+
   return (
-    <TypesafeRouter routes={routes} history={history} fallback={NotFound} />
+    <ThemeProvider theme={themes.light}>
+      <Paper className={classes.root}>
+        <TopBar title="Manager for Exhibition" isNeedBackButton={false} />
+        <TypesafeRouter routes={routes} history={history} fallback={NotFound} />
+        <div className={classes.bottomNav}>
+          <BottomNav />
+        </div>
+      </Paper>
+    </ThemeProvider>
   );
 };
 
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,46 @@
 import React from "react";
+import {
+  Card,
+  CardContent,
+  Typography,
+  CardActions,
+  Button,
+} from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles({
+  root: {
+    padding: "1rem",
+  },
+  title: {
+    marginBottom: "8px",
+  },
+});
 
 const Home: React.FunctionComponent = () => {
-  return <div>Home</div>;
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <Card variant="outlined">
+        <CardContent>
+          <Typography variant="h6" component="h2" className={classes.title}>
+            入退室 QRコードスキャン
+          </Typography>
+          <Typography variant="body2" paragraph={true}>
+            自展示への入退室者のリストバンドのQRコードを読み取ることで、入退室処理を行えます。
+          </Typography>
+          <Typography variant="body2" paragraph={true}>
+            画面に案内が出た場合は、表示された内容を来場者に案内してください。
+          </Typography>
+        </CardContent>
+        <CardActions>
+          <Button color="secondary">入室スキャン</Button>
+          <Button color="secondary">退室スキャン</Button>
+        </CardActions>
+      </Card>
+    </div>
+  );
 };
 
 export default Home;
