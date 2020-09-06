1a72218ae6a3bd39cf0989ffc7ad0bf3ce1c3e6d
subaru <s@su8ru.dev>
2020-09-06 17:10:10 +0900

    feat: provide context from App

--- src/App.tsx
+++ src/App.tsx
@@ -4,17 +4,28 @@ import routes from "@/libs/routes";
 import { createBrowserHistory } from "history";
 import NotFound from "@/pages/NotFound";
 import MainLayout from "@/layouts/Main";
+import Auth, { AuthContext } from "@/libs/auth";
 
 const App: React.FunctionComponent = () => {
   const [history] = useState(createBrowserHistory());
+  const [provideVal, setProvideVal] = React.useState(() => ({
+    val: new Auth(),
+  }));
+  React.useEffect(() => {
+    provideVal.val.on_change(() => {
+      setProvideVal((old) => ({ ...old }));
+    });
+  }, []);
 
   return (
-    <TypesafeRouter
-      routes={routes}
-      history={history}
-      layout={MainLayout}
-      fallback={NotFound}
-    />
+    <AuthContext.Provider value={provideVal}>
+      <TypesafeRouter
+        routes={routes}
+        history={history}
+        layout={MainLayout}
+        fallback={NotFound}
+      />
+    </AuthContext.Provider>
   );
 };
 
