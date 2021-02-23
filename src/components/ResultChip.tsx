33e2dd2db987b3a989fdead3f129fc22a5258d91
subaru <s@su8ru.dev>
2021-02-23 23:26:30 +0900

    feat: create ResultChip component

---
+++ src/components/ResultChip.tsx
@@ -0,0 +1,62 @@
+import React, { useEffect, useState } from "react";
+import { Chip, Fade, SvgIcon } from "@material-ui/core";
+import { CheckCircleOutline, ErrorOutline } from "@material-ui/icons";
+import { makeStyles } from "@material-ui/core/styles";
+import clsx from "clsx";
+
+const useStyles = makeStyles({
+  root: {
+    color: "#fff",
+    "& .MuiChip-icon, .MuiChip-deleteIcon": {
+      color: "#fff",
+    },
+  },
+  success: {
+    backgroundColor: "#4caf50",
+  },
+  error: {
+    backgroundColor: "#f44336",
+  },
+});
+
+export type ResultColor = "success" | "error";
+
+export interface ResultChipProps {
+  onDelete?: () => void;
+  color: ResultColor;
+  message: string;
+}
+
+const getResultIcon = (
+  color: ResultColor
+): React.ReactElement<typeof SvgIcon> => {
+  return color == "success" ? <CheckCircleOutline /> : <ErrorOutline />;
+};
+
+const ResultChip: React.FC<ResultChipProps> = (props) => {
+  const classes = useStyles();
+  const [fade, setFade] = useState(true);
+
+  useEffect(() => {
+    setFade(false);
+  }, [props.color, props.message]);
+  useEffect(() => {
+    if (!fade) setFade(true);
+  }, [fade]);
+
+  return (
+    <Fade in={fade} timeout={{ appear: 0, enter: 500, exit: 0 }}>
+      <Chip
+        onDelete={props.onDelete}
+        icon={getResultIcon(props.color)}
+        label={props.message}
+        className={clsx(
+          classes.root,
+          props.color === "success" ? classes.success : classes.error
+        )}
+      />
+    </Fade>
+  );
+};
+
+export default ResultChip;
