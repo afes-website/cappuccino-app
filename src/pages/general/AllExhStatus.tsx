0c68518c494d01b2d0022319dcd9eba5bfc35256
subaru <s@su8ru.dev>
2021-03-23 14:54:31 +0900

    feat: sort exh list

--- src/pages/general/AllExhStatus.tsx
+++ src/pages/general/AllExhStatus.tsx
@@ -10,14 +10,16 @@ import {
   Typography,
   useTheme,
   Theme,
+  ButtonGroup,
+  Button,
 } from "@material-ui/core";
 import { createStyles, makeStyles } from "@material-ui/core/styles";
+import PullToRefresh from "@/components/PullToRefresh";
+import { useWristBandPaletteColor } from "@/libs/wristBandColor";
 import { AuthContext, useVerifyPermission } from "@/libs/auth";
 import { useTitleSet } from "@/libs/title";
 import api, { AllStatus, ExhStatus, Terms } from "@afes-website/docs";
 import aspida from "@aspida/axios";
-import { useWristBandPaletteColor } from "@/libs/wristBandColor";
-import PullToRefresh from "@/components/PullToRefresh";
 
 const useStyles = makeStyles((theme) =>
   createStyles({
@@ -30,6 +32,13 @@ const useStyles = makeStyles((theme) =>
       display: "flex",
       justifyContent: "center",
       alignItems: "center",
+    },
+    sortKeySelector: {
+      margin: `0 ${theme.spacing(1)}px`,
+      width: `calc(100% - ${theme.spacing(2)}px)`,
+    },
+    sortMessage: {
+      marginTop: theme.spacing(1.5),
     },
     listItem: {
       position: "relative",
@@ -49,6 +58,9 @@ const useStyles = makeStyles((theme) =>
   })
 );
 
+const sortKeys = ["roomId", "count", "congestion"] as const;
+type SortKey = typeof sortKeys[number];
+
 const AllExhStatus: React.FC = () => {
   useTitleSet("全展示の滞在状況一覧");
   useVerifyPermission("general");
@@ -58,6 +70,8 @@ const AllExhStatus: React.FC = () => {
 
   const [status, setStatus] = useState<AllStatus | null>(null);
   const [terms, setTerms] = useState<Terms | null>(null);
+
+  const [sortKey, setSortKey] = useState<SortKey>("roomId");
 
   const fetch = useCallback(
     () =>
@@ -99,19 +113,40 @@ const AllExhStatus: React.FC = () => {
     .map((exh) => exh.limit)
     .reduce((a, b) => Math.max(a, b));
 
+  const onSortKeyChange = (e: React.MouseEvent<HTMLButtonElement>) => {
+    const val: string = e.currentTarget.value;
+    if ((sortKeys as ReadonlyArray<string>).includes(val))
+      setSortKey(val as SortKey);
+  };
+
   return (
     <PullToRefresh onRefresh={fetch}>
       <div className={classes.root}>
-        <Typography align="center" variant="body2" color="textSecondary">
-          展示番号順に、すべての展示を表示しています。
+        <ButtonGroup fullWidth className={classes.sortKeySelector}>
+          {Object.entries(sortOptions).map(([key, { label }]) => (
+            <Button
+              key={key}
+              value={key}
+              color={key === sortKey ? "primary" : "secondary"}
+              variant={key === sortKey ? "contained" : "outlined"}
+              onClick={onSortKeyChange}
+              fullWidth
+            >
+              {label}順
+            </Button>
+          ))}
+        </ButtonGroup>
+        <Typography
+          align="center"
+          variant="body2"
+          color="textSecondary"
+          className={classes.sortMessage}
+        >
+          {sortOptions[sortKey].message}
         </Typography>
         <List>
           {Object.entries(status.exh)
-            .sort(([, a], [, b]) => {
-              if (a.info.room_id < b.info.room_id) return -1;
-              if (a.info.room_id > b.info.room_id) return 1;
-              return 0;
-            })
+            .sort(sortOptions[sortKey].compareFn)
             .map(([exhId, exhStatus]) => (
               <ListItem key={exhId} divider className={classes.listItem}>
                 <ListItemIcon>
@@ -152,6 +187,55 @@ const AllExhStatus: React.FC = () => {
       </div>
     </PullToRefresh>
   );
+};
+
+const sortOptions: {
+  [key in SortKey]: {
+    label: string;
+    message: string;
+    compareFn: (a: [string, ExhStatus], b: [string, ExhStatus]) => number;
+  };
+} = {
+  roomId: {
+    label: "展示番号",
+    message: "展示番号順に、すべての展示を表示しています。",
+    compareFn: ([, a], [, b]) => {
+      if (a.info.room_id < b.info.room_id) return -1;
+      if (a.info.room_id > b.info.room_id) return 1;
+      return 0;
+    },
+  },
+  count: {
+    label: "滞在人数",
+    message: "滞在人数が多い順に、すべての展示を表示しています。",
+    compareFn: ([, a], [, b]) => {
+      const sum_a = Object.values(a.count).reduce(
+        (prev, curr) => prev + curr,
+        0
+      );
+      const sum_b = Object.values(b.count).reduce(
+        (prev, curr) => prev + curr,
+        0
+      );
+      return (sum_a - sum_b) * -1;
+    },
+  },
+  congestion: {
+    label: "混雑度",
+    message: "空き枠が少ない順に、すべての展示を表示しています。",
+    compareFn: ([, a], [, b]) => {
+      const sum_a = Object.values(a.count).reduce(
+        (prev, curr) => prev + curr,
+        0
+      );
+      const sum_b = Object.values(b.count).reduce(
+        (prev, curr) => prev + curr,
+        0
+      );
+      // ソート順序: (上限人数 - 滞在人数) 等しい場合は人数が多いほうが混雑
+      return a.limit - sum_a - (b.limit - sum_b) || sum_b - sum_a;
+    },
+  },
 };
 
 export default AllExhStatus;
