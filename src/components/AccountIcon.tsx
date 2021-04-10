1605bc9ff8a25b14d01965a09777669bf7cff4ff
subaru <s@su8ru.dev>
2021-04-10 20:55:45 +0900

    feat: expand account icon variation

--- src/components/AccountIcon.tsx
+++ src/components/AccountIcon.tsx
@@ -1,7 +1,12 @@
 import React from "react";
 import { StorageUserInfo } from "libs/auth";
 import { Person } from "@material-ui/icons";
-import { PersonSetting, PersonSecurity } from "components/MaterialSvgIcons";
+import {
+  PersonSetting,
+  PersonSecurity,
+  PersonAssignment,
+  PersonRoom,
+} from "components/MaterialSvgIcons";
 import { SvgIconProps } from "@material-ui/core";
 
 const AccountIcon: React.FC<
@@ -9,8 +14,10 @@ const AccountIcon: React.FC<
 > = (props) => {
   const { account, ...iconProps } = props;
   if (account?.permissions.admin) return <PersonSecurity {...iconProps} />;
+  if (account?.permissions.reservation)
+    return <PersonAssignment {...iconProps} />;
   if (account?.permissions.general) return <PersonSetting {...iconProps} />;
-  if (account?.permissions.exhibition) return <Person {...iconProps} />;
+  if (account?.permissions.exhibition) return <PersonRoom {...iconProps} />;
   return <Person {...iconProps} />;
 };
 
