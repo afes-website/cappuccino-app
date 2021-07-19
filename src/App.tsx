c6b53f900bcc99cf2e2fe637b728461f2f341ee2
subaru <s@su8ru.dev>
2021-07-20 00:13:56 +0900

    refactor: replace context with auth state and dispatch

--- src/App.tsx
+++ src/App.tsx
@@ -6,23 +6,20 @@ import MainLayout from "layouts/Main";
 import TabletLayout from "layouts/Tablet";
 import NotFound from "pages/NotFound";
 import { createBrowserHistory } from "history";
-import Auth, { AuthContext } from "libs/auth";
+import useAuthProvideValue from "libs/auth/useAuthProvideValue";
+import {
+  AuthStateContextProvider,
+  AuthDispatchContextProvider,
+} from "libs/auth/useAuth";
 import routes from "libs/routes";
 
 const App: React.VFC = () => {
   const [history] = useState(createBrowserHistory());
-  const [provideVal, setProvideVal] = React.useState(() => ({
-    val: new Auth(),
-  }));
-  React.useEffect(() => {
-    provideVal.val.on_change(() => {
-      setProvideVal((old) => ({ ...old }));
-    });
-  }, [provideVal.val]);
+  const [authState, authDispatch] = useAuthProvideValue();
 
   const redirect_to_login = () => {
     if (
-      !provideVal.val.get_current_user_id() &&
+      !authState.currentUserId &&
       ![routes.Login.route.create({}), routes.Terms.route.create({})].includes(
         history.location.pathname
       )
@@ -30,7 +27,7 @@ const App: React.VFC = () => {
       history.push(routes.Login.route.create({}));
     }
   };
-  React.useEffect(redirect_to_login, [history, provideVal]);
+  React.useEffect(redirect_to_login, [authState.currentUserId, history]);
   React.useEffect(() => {
     return history.listen(redirect_to_login);
   });
@@ -46,14 +43,16 @@ const App: React.VFC = () => {
   }, []);
 
   return (
-    <AuthContext.Provider value={provideVal}>
-      <TypesafeRouter
-        routes={routes}
-        history={history}
-        layout={LayoutWithProviders}
-        fallback={NotFound}
-      />
-    </AuthContext.Provider>
+    <AuthStateContextProvider value={authState}>
+      <AuthDispatchContextProvider value={authDispatch}>
+        <TypesafeRouter
+          routes={routes}
+          history={history}
+          layout={LayoutWithProviders}
+          fallback={NotFound}
+        />
+      </AuthDispatchContextProvider>
+    </AuthStateContextProvider>
   );
 };
 
