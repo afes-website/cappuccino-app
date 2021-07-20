8e7d9583eb7c0802bd7f821a48a96b22dd729ea7
subaru <s@su8ru.dev>
2021-07-20 21:49:26 +0900

    feat: add ReloadButton

---
+++ src/components/ReloadButton.tsx
@@ -0,0 +1,79 @@
+import React, { useState } from "react";
+import {
+  Box,
+  Button,
+  CircularProgress,
+  createStyles,
+  makeStyles,
+  Theme,
+  Typography,
+} from "@material-ui/core";
+import { Refresh } from "@material-ui/icons";
+import { Moment } from "moment";
+import clsx from "clsx";
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      display: "flex",
+      flexDirection: "column",
+      alignItems: "flex-end",
+    },
+    lastUpdated: {
+      marginRight: theme.spacing(0.5),
+    },
+  })
+);
+
+interface Props {
+  onClick: () => Promise<void>;
+  lastUpdated?: Moment | null;
+  className?: string;
+}
+
+const ReloadButton: React.VFC<Props> = ({
+  onClick,
+  lastUpdated,
+  className,
+}) => {
+  const classes = useStyles();
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+
+  return (
+    <Box className={clsx(classes.root, className)}>
+      <Button
+        variant="text"
+        color="secondary"
+        size="small"
+        disabled={isLoading}
+        onClick={() => {
+          setIsLoading(true);
+          onClick().finally(() => {
+            setIsLoading(false);
+          });
+        }}
+        startIcon={
+          isLoading ? (
+            <CircularProgress color="inherit" size={12} thickness={5} />
+          ) : (
+            <Refresh />
+          )
+        }
+      >
+        再読み込み
+      </Button>
+      {lastUpdated && (
+        <Typography
+          align="right"
+          variant="body2"
+          color="textSecondary"
+          className={classes.lastUpdated}
+        >
+          最終更新: {lastUpdated.format("M/D HH:mm:ss")}
+        </Typography>
+      )}
+    </Box>
+  );
+};
+
+export default ReloadButton;
