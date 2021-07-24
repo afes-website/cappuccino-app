50e858e65a35f1c109a390026ad98207f298c30e
subaru <s@su8ru.dev>
2021-07-24 23:09:52 +0900

    feat: create useExhibitionImageUrl

---
+++ src/libs/useExhibitionImageUrl.tsx
@@ -0,0 +1,38 @@
+import api from "@afes-website/docs";
+import aspida from "@aspida/axios";
+import { useEffect, useState } from "react";
+
+let imageIds: { [exhId: string]: string } | null = null;
+
+const useExhibitionImageUrl = (
+  exhId: string,
+  query?: { size?: "s" | "m" }
+): string | null => {
+  const [imageId, setImageId] = useState<string | null>(null);
+
+  useEffect(() => {
+    if (imageIds) setImageId(exhId in imageIds ? imageIds[exhId] : null);
+  }, [exhId]);
+
+  useEffect(() => {
+    if (!imageIds)
+      api(aspida())
+        .exhibitions.$get({
+          headers: { Authorization: "" },
+        })
+        .then((allStatus) => {
+          imageIds = Object.fromEntries(
+            Object.entries(allStatus.exhibition).map(([id, status]) => [
+              id,
+              status.info.thumbnail_image_id,
+            ])
+          );
+          if (exhId in imageIds) setImageId(imageIds[exhId]);
+        });
+  }, [exhId]);
+
+  if (!imageIds) return null;
+  return imageId ? api(aspida()).images._id(imageId).$path({ query }) : null;
+};
+
+export default useExhibitionImageUrl;
