dbbb0102fcf7fb8a91c99f5d00dd163456198d75
subaru <s@su8ru.dev>
2021-07-27 22:16:18 +0900

    feat: retain promise in global

--- src/libs/useExhibitionImageUrl.tsx
+++ src/libs/useExhibitionImageUrl.tsx
@@ -1,35 +1,45 @@
 import api from "@afes-website/docs";
 import aspida from "@aspida/axios";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 
 let imageIds: { [exhId: string]: string } | null = null;
+let imageIdsFetchPromise: Promise<unknown> | null = null;
 
 const useExhibitionImageUrl = (
   exhId: string,
   query?: { size?: "s" | "m" }
 ): string | null => {
-  const [imageId, setImageId] = useState<string | null>(null);
+  const [fetchStatus, setFetchStatus] = useState<
+    "beforeInit" | "waitingPromise" | "ready"
+  >("beforeInit");
 
-  useEffect(() => {
-    if (imageIds) setImageId(exhId in imageIds ? imageIds[exhId] : null);
-  }, [exhId]);
+  console.log(imageIdsFetchPromise);
 
-  useEffect(() => {
-    if (!imageIds)
-      api(aspida())
-        .exhibitions.$get()
-        .then((allStatus) => {
-          imageIds = Object.fromEntries(
-            Object.entries(allStatus.exhibition).map(([id, status]) => [
-              id,
-              status.info.thumbnail_image_id,
-            ])
-          );
-          if (exhId in imageIds) setImageId(imageIds[exhId]);
-        });
-  }, [exhId]);
-
-  return imageId ? api(aspida()).images._id(imageId).$path({ query }) : null;
+  switch (fetchStatus) {
+    case "beforeInit":
+      if (!imageIdsFetchPromise) {
+        imageIdsFetchPromise = api(aspida())
+          .exhibitions.$get()
+          .then((allStatus) => {
+            imageIds = Object.fromEntries(
+              Object.entries(allStatus.exhibition).map(([id, status]) => [
+                id,
+                status.info.thumbnail_image_id,
+              ])
+            );
+          });
+      }
+      imageIdsFetchPromise.then(() => {
+        setFetchStatus("ready");
+      });
+      setFetchStatus("waitingPromise");
+      return null;
+    case "waitingPromise":
+      return null;
+    case "ready":
+      if (!imageIds || !{}.hasOwnProperty.call(imageIds, exhId)) return null;
+      return api(aspida()).images._id(imageIds[exhId]).$path({ query });
+  }
 };
 
 export default useExhibitionImageUrl;
