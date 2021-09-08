7b5cc7a3f461d77b44a97fca2fdcc9ceef0d1a9e
subaru <s@su8ru.dev>
2021-09-09 00:22:02 +0900

    feat: confirm login

--- src/pages/LoginQR.tsx
+++ src/pages/LoginQR.tsx
@@ -1,21 +1,18 @@
 import React, { useState } from "react";
 import { useHistory } from "react-router-dom";
-import {
-  Card,
-  CardContent,
-  List,
-  ListItem,
-  ListItemText,
-} from "@material-ui/core";
+import { Button, Card, CardContent, Typography } from "@material-ui/core";
+import { Alert } from "@material-ui/lab";
 import { createStyles, makeStyles } from "@material-ui/core/styles";
-import api, { UserInfo } from "@afes-website/docs";
+import api from "@afes-website/docs";
 import axios from "@aspida/axios";
+import AccountIcon from "components/AccountIcon";
 import CardList from "components/CardList";
 import ErrorDialog from "components/ErrorDialog";
 import QRScanner from "components/QRScanner";
 import isAxiosError from "libs/isAxiosError";
 import { useTitleSet } from "libs/title";
-import { useAuthDispatch } from "libs/auth/useAuth";
+import { StorageUserInfo } from "libs/auth/@types";
+import { useAuthDispatch, useAuthState } from "libs/auth/useAuth";
 import routes from "libs/routes";
 import { StatusColor } from "types/statusColor";
 
@@ -25,19 +22,56 @@ const useStyles = makeStyles((theme) =>
       padding: "0 !important",
       objectFit: "cover",
     },
+    confirm: {
+      display: "flex",
+      flexDirection: "column",
+      alignItems: "center",
+      justifyContent: "center",
+      marginTop: "calc(var(--100vh) * 0.45 - 50%)",
+      "& > * + *": {
+        marginTop: theme.spacing(4),
+      },
+    },
+    userInfo: {
+      display: "flex",
+      flexDirection: "column",
+      alignItems: "center",
+      "& > * + *": {
+        marginTop: theme.spacing(0.5),
+      },
+      "& > *:first-child": {
+        marginBottom: theme.spacing(1),
+      },
+    },
+    userIcon: {
+      width: 80,
+      height: 80,
+    },
+    login: {
+      display: "flex",
+      flexDirection: "column",
+      "& > * + *": {
+        marginTop: theme.spacing(1.5),
+      },
+      boxSizing: "border-box",
+      width: "100%",
+      padding: "0 24px",
+    },
   })
 );
 
 const LoginQR: React.VFC = () => {
   useTitleSet("QRコードでログイン");
+
   const classes = useStyles();
   const history = useHistory();
+  const { currentUserId } = useAuthState();
   const { registerUser } = useAuthDispatch();
+
   const [checkStatus, setCheckStatus] = useState<
     StatusColor | "confirm" | null
   >(null);
-  const [user, setUser] = useState<UserInfo | null>(null);
-  const [id, setId] = useState("");
+  const [user, setUser] = useState<StorageUserInfo | null>(null);
   const [encoded, setEncoded] = useState("");
   const [errorText, setErrorText] = useState<string[]>([]);
   const [dialogOpen, setDialogOpen] = useState(false);
@@ -83,7 +117,7 @@ const LoginQR: React.VFC = () => {
     ) {
       const id = obj.id;
       const pw = obj.pw;
-      setId(id);
+
       if (typeof id === "string" && typeof pw === "string") {
         api(axios())
           .auth.login.$post({ body: { id: id, password: pw } })
@@ -96,7 +130,7 @@ const LoginQR: React.VFC = () => {
               })
               .then((Info) => {
                 setCheckStatus("confirm");
-                setUser(Info);
+                setUser({ ...Info, token: res.token });
               })
               .catch((e) => {
                 setCheckStatus("error");
@@ -106,12 +140,6 @@ const LoginQR: React.VFC = () => {
                   (isAxiosError(e) && e.response?.data.message) || e.message,
                 ]);
               });
-
-            // registerUser(res.token).then(() => {
-            //   setColorStatus("success");
-            //   if (gtag) gtag("event", "login");
-            //   history.push(routes.Home.route.create({}));
-            // });
           })
           .catch((e) => {
             setCheckStatus("error");
@@ -138,61 +166,82 @@ const LoginQR: React.VFC = () => {
     }
   };
 
+  const confirmLogin = () => {
+    if (user)
+      registerUser(user.token).then(() => {
+        if (gtag) gtag("event", "login");
+        history.push(routes.Home.route.create({}));
+      });
+  };
+
+  const cancelLogin = () => {
+    history.push(routes[currentUserId ? "Home" : "Login"].route.create({}));
+  };
+
   return (
     <div>
       {checkStatus === "confirm" ? (
-        <CardList>
-          <Card>
-            <CardContent className={classes.noPadding}>
-              <List>
-                <ListItem>
-                  <ListItemText
-                    primary={user ? user.id : "-"}
-                    secondary="User Id"
-                  />
-                </ListItem>
-                <ListItem>
-                  {/*ListItemIcon*/}
-                  <ListItemText
-                    primary={user ? user.name : "-"}
-                    secondary="User Name"
-                  />
-                </ListItem>
-              </List>
-            </CardContent>
-          </Card>
-        </CardList>
+        <div className={classes.confirm}>
+          {user ? (
+            <>
+              <Typography variant="h5">ログインしますか？</Typography>
+              <div className={classes.userInfo}>
+                <AccountIcon account={user} className={classes.userIcon} />
+                <Typography variant="h6">{user.name}</Typography>
+                <Typography variant="body2">@{user.id}</Typography>
+              </div>
+              <div className={classes.login}>
+                <Button
+                  variant="contained"
+                  color="primary"
+                  fullWidth={true}
+                  onClick={confirmLogin}
+                >
+                  ログイン
+                </Button>
+                <Button
+                  variant="text"
+                  color="secondary"
+                  size="small"
+                  fullWidth={true}
+                  onClick={cancelLogin}
+                >
+                  キャンセル
+                </Button>
+              </div>
+            </>
+          ) : (
+            <Typography>ユーザーが見つかりませんでした。</Typography>
+          )}
+        </div>
       ) : (
-        <CardList>
-          <Card>
-            <CardContent className={classes.noPadding}>
-              <QRScanner
-                onScanFunc={handleScan}
-                videoStop={false}
-                color={checkStatus ?? undefined}
-              />
-            </CardContent>
-          </Card>
-          <Card>
-            <CardContent className={classes.noPadding}>
-              <List>
-                <ListItem>
-                  {/*ListItemIcon*/}
-                  <ListItemText primary={id ? id : "-"} secondary="User Id" />
-                </ListItem>
-              </List>
-            </CardContent>
-          </Card>
-          <Card>
-            <ErrorDialog
-              open={dialogOpen}
-              message={errorText}
-              onClose={() => {
-                setDialogOpen(false);
-              }}
-            />
-          </Card>
-        </CardList>
+        <>
+          <CardList>
+            <Card>
+              <CardContent className={classes.noPadding}>
+                <QRScanner
+                  onScanFunc={handleScan}
+                  videoStop={false}
+                  color={checkStatus ?? undefined}
+                />
+              </CardContent>
+            </Card>
+            <Card>
+              <Alert severity="info">
+                <Typography variant="body2">
+                  配布されたQRコードをスキャンしてください。
+                </Typography>
+              </Alert>
+            </Card>
+          </CardList>
+          <ErrorDialog
+            open={dialogOpen}
+            message={errorText}
+            onClose={() => {
+              setDialogOpen(false);
+            }}
+          />
+        </>
       )}
     </div>
   );
