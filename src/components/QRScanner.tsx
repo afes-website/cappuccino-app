3bf1df1a67f57e8cb1b2400153a4871da223ff4b
subaru <s@su8ru.dev>
2021-09-10 00:08:39 +0900

    feat: set delay 1 (ms)

--- src/components/QRScanner.tsx
+++ src/components/QRScanner.tsx
@@ -192,7 +192,7 @@ const QRScanner: React.VFC<QRScannerProps> = (props) => {
             onLoad={() => {
               setScannerStatus("waiting");
             }}
-            delay={0}
+            delay={1}
             showViewFinder={false}
             facingMode="environment"
             constraints={{ deviceId: videoDeviceId, facingMode: "environment" }}
