94c9d8d6f8f9d29698d4e47165e4df43b63c7820
Takeno-hito <18237819+Takeno-hito@users.noreply.github.com>
2021-11-09 11:41:58 +0900

    feat: warning alert when exhibition is full

--- src/components/GuestScan.tsx
+++ src/components/GuestScan.tsx
@@ -1,5 +1,5 @@
-import React, { useEffect, useRef, useState } from "react";
-import { Guest } from "@afes-website/docs";
+import React, { useCallback, useEffect, useRef, useState } from "react";
+import api, { Guest } from "@afes-website/docs";
 import clsx from "clsx";
 import {
   Card,
@@ -22,6 +22,8 @@ import ResultChip, { ResultChipRefs } from "components/ResultChip";
 import { ExhStatusCard } from "components/StayStatusCard";
 import useErrorHandler from "hooks/useErrorHandler";
 import { StatusColor } from "types/statusColor";
+import { useAspidaClient, useAuthState } from "hooks/auth/useAuth";
+import { Alert } from "@material-ui/lab";
 
 const useStyles = makeStyles((theme) =>
   createStyles({
@@ -71,6 +73,7 @@ const GuestScan: React.VFC<Props> = ({ handleScan, page }) => {
   const [latestGuestId, setLatestGuestId] = useState("");
   const [opensGuestInputModal, setOpensGuestInputModal] = useState(false);
   const [checkStatus, setCheckStatus] = useState<StatusColor | null>(null);
+  const [isFull, setIsFull] = useState<boolean>(false);
 
   // エラー処理
   const [errorMessage, setError] = useErrorHandler();
@@ -94,13 +97,49 @@ const GuestScan: React.VFC<Props> = ({ handleScan, page }) => {
       setLatestGuestId(guestId);
       try {
         await handleScan(guestId);
-        setCheckStatus("success");
+        checkIsFull().then((v) => {
+          if (v) setCheckStatus("warning");
+          else setCheckStatus("success");
+        });
       } catch (e) {
         setCheckStatus("error");
         setError(e);
       }
     }
   };
+
+  const { currentUser } = useAuthState();
+  const aspida = useAspidaClient();
+
+  const checkIsFull = useCallback(
+    () =>
+      api(aspida)
+        .exhibitions._id(currentUser?.id || "")
+        .$get()
+        .then((status) => {
+          const sum = Object.entries(status.count)
+            .map(([, value]) => value)
+            .reduce((prev, curr) => prev + curr, 0);
+
+          if (sum >= status.capacity) {
+            setIsFull(true);
+            if (checkStatus === null) setCheckStatus("warning");
+            return true;
+          } else {
+            if (checkStatus === "warning") setCheckStatus(null);
+            setIsFull(false);
+            return false;
+          }
+        }),
+    [aspida, checkStatus, currentUser?.id]
+  );
+
+  useEffect(() => {
+    if (!isExh) return;
+    checkIsFull();
+    const intervalId = setInterval(checkIsFull, 15000);
+    return () => clearInterval(intervalId);
+  }, [isExh, checkIsFull]);
 
   useEffect(() => {
     switch (checkStatus) {
@@ -114,6 +153,17 @@ const GuestScan: React.VFC<Props> = ({ handleScan, page }) => {
           setLatestGuestId("");
         }, 3000);
         if (resultChipRef.current)
+          resultChipRef.current.open(
+            "success",
+            `${actionName}成功 / ゲスト ID: ${latestGuestId}`,
+            3000
+          );
+        break;
+      case "warning":
+        setTimeout(() => {
+          setLatestGuestId("");
+        }, 3000);
+        if (resultChipRef.current && latestGuestId)
           resultChipRef.current.open(
             "success",
             `${actionName}成功 / ゲスト ID: ${latestGuestId}`,
@@ -158,6 +208,15 @@ const GuestScan: React.VFC<Props> = ({ handleScan, page }) => {
               <Card>
                 <CardContent className={classes.noPadding}>
                   <ErrorAlert errorMessage={errorMessage} />
+                </CardContent>
+              </Card>
+            )}
+
+            {/* Warning */}
+            {isFull && (
+              <Card>
+                <CardContent className={classes.noPadding}>
+                  <Alert severity="warning">展示内は満員です。</Alert>
                 </CardContent>
               </Card>
             )}
